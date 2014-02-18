8c9d8743159e6c6b8b172c8390f2185af34e6455
Jordan Brengman <jordanbrengman@gmail.com>
2014-02-18 09:26:08 -0800

    Move code to main() function

--- echo_client.py
+++ echo_client.py
@@ -1,10 +1,18 @@
 import socket
+import sys
 
-message = raw_input('Enter a message to send and recieve: ')
+def main():
+	if (len(sys.argv) > 1 and type(sys.argv[1]) == str):
+		message = sys.argv[1]
+	else:
+		message = raw_input('Enter a message to send and recieve: ')
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 50000))
-s.sendall(message)
-recieved_message = s.recv(1024)
-s.close()
-print(recieved_message)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(('127.0.0.1', 50000))
+	s.sendall(message)
+	recieved_message = s.recv(1024)
+	s.close()
+	print(recieved_message)
+
+if __name__ == '__main__':
+	main()
--- echo_server.py
+++ echo_server.py
@@ -1,11 +1,15 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('127.0.0.1', 50000))
-s.listen(5)
-while (True):
-	connection, address = s.accept()
-	message = connection.recv(1024)
-	if message:
-		connection.send(message)
-	connection.close()
+def main():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(('127.0.0.1', 50000))
+	s.listen(5)
+	while (True):
+		connection, address = s.accept()
+		message = connection.recv(1024)
+		if message:
+			connection.send(message)
+		connection.close
+
+if __name__ == '__main__':
+	main()
